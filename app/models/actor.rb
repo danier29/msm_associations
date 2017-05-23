ab769ed553d8b868d983a13b46a72914157a8736
Danielle Calderone <danier29@users.noreply.github.com>
2017-05-23 00:18:49 -0500

    as of 12:19pm

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,4 +7,6 @@ class Actor < ApplicationRecord
   # - bio: no rules
   # - image_url: no rules
 
+    has_many(:characters, :class_name => "Character", :foreign_key => "actor_id")
+    has_many(:movie, :class_name => "Movie", :foreign_key => "actor_id")
 end
--- app/models/director.rb
+++ app/models/director.rb
@@ -7,6 +7,6 @@ validates :name, :presence => true, :uniqueness => {:scope => :dob}
 # - bio: no rules
 # - image_url: no rules
 
-has_many(:movie, :class_name => "Movie", :foreign_key => "movie_id")
+has_many(:movie, :class_name => "Movie", :foreign_key => "director_id")
 
 end
